457aa870455730da6c555cb9d3f6130ae22588a1
lirc572 <lirc572@gmail.com>
2022-08-26 01:08:20 +0800

    fix: fix url pattern matching

--- src/main.tsx
+++ src/main.tsx
@@ -4,28 +4,30 @@ import * as colors from 'twind/colors'
 import { App } from './app'
 import './main.css'
 
-setup({
-    props: {
-        // tw: false, // to disable
-        // css: false, // to disable
-        className: true, // to enable
-    },
-    theme: {
-        extend: {
-            colors,
+if (/rfc\d+(.html)?$/.test(window.location.pathname)) {
+    setup({
+        props: {
+            // tw: false, // to disable
+            // css: false, // to disable
+            className: true, // to enable
         },
-    },
-    mode: silent,
-})
+        theme: {
+            extend: {
+                colors,
+            },
+        },
+        mode: silent,
+    })
 
-document.documentElement.style.scrollBehavior = 'smooth';
+    document.documentElement.style.scrollBehavior = 'smooth';
 
-const body = document.querySelector('body')
-const app = document.createElement('div')
-app.id = 'rfc-helper-root'
+    const body = document.querySelector('body')
+    const app = document.createElement('div')
+    app.id = 'rfc-helper-root'
 
-if (body) {
-    body.append(app)
+    if (body) {
+        body.append(app)
+    }
+
+    render(<App />, document.getElementById(app.id) as HTMLElement)
 }
-
-render(<App />, document.getElementById(app.id) as HTMLElement)
